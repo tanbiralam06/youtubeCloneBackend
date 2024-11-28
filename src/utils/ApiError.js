f6a55c54077ebf6d1a45daa400cfea9174b42b2d
Tanbir Alam <Tanbir>
2024-11-28 18:52:37 +0530

    project full set up

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         statusCode,
         message= "Kuch to garbar Hein",
         errors = [],
-        statck = ""
+        stack = ""
 
     ){
         super(message)
@@ -15,8 +15,8 @@ class ApiError extends Error {
 
 
 
-        if(statck) {
-            this.stack = statck
+        if(stack) {
+            this.stack = stack
         } else{
             Error.captureStackTrace(this, this.constructor)
         }
